cd8fe7dfc07d690979e6b89a88a80f7ed47a226d
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-04-01 22:40:30 +0530

    sidebar-left now saves the scroll position

--- themes/basic/assets/js/sidebar/left/state.js
+++ themes/basic/assets/js/sidebar/left/state.js
@@ -10,6 +10,7 @@ function generateKey(tag) {
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOMContentLoaded!");
     const details = document.querySelectorAll(".sidebar-left details");
+    const sidebarLeft = document.querySelector("div.sidebar-left");
 
     /* restore state */
     details.forEach((tag) => {
@@ -19,11 +20,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    const scrollTop = sessionStorage.getItem("sidebar-left-scroll-top");
+    if (scrollTop != null) {
+        sidebarLeft.scrollTop = parseFloat(scrollTop);
+    }
+
     /* save states */
     details.forEach((tag) => {
         tag.addEventListener("toggle", function () {
             console.log("toggle");
             sessionStorage.setItem(generateKey(tag), tag.open);
         });
+    });
+
+    window.addEventListener("beforeunload", function () {
+        console.log("scroll-top:", sidebarLeft.scrollTop);
+        sessionStorage.setItem("sidebar-left-scroll-top", sidebarLeft.scrollTop);
     });
 });
